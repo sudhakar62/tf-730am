68cdf685d9c1838b14acbb815895af8f8adf8d4e
sudhakar62 <sudhakar6294@gmail.com>
2023-09-17 01:19:41 +0530

    create public security group

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -132,3 +132,37 @@ resource "aws_network_acl" "ecomm-pvt-nacl" {
     Name = "ecomm-private-nacl"
   }
 }
+
+#Public Security Group
+resource "aws_security_group" "ecomm-pub-sg" {
+  name        = "ecomm-web"
+  description = "Allow SSH & HTTP inbound traffic"
+  vpc_id      = aws_vpc.ecomm.id
+
+  ingress {
+    description      = "allow SSH from WWW"
+    from_port        = 22
+    to_port          = 22 
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "allow HTTP from WWW"
+    from_port        = 80
+    to_port          = 80 
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ecomm-public-firewall"
+  }
+}
