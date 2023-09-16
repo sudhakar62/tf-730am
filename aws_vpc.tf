ea251fc5ae037a3d2af5fbf0e05f49b6700e1202
sudhakar62 <sudhakar6294@gmail.com>
2023-09-17 01:27:33 +0530

    create private security group

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -166,3 +166,37 @@ resource "aws_security_group" "ecomm-pub-sg" {
     Name = "ecomm-public-firewall"
   }
 }
+
+#Private Security Group
+resource "aws_security_group" "ecomm-pvt-sg" {
+  name        = "ecomm-db"
+  description = "Allow SSH & MYSQL inbound traffic"
+  vpc_id      = aws_vpc.ecomm.id
+
+  ingress {
+    description      = "allow SSH from WWW"
+    from_port        = 22
+    to_port          = 22 
+    protocol         = "tcp"
+    cidr_blocks      = ["10.0.0.0/16"]
+  }
+
+  ingress {
+    description      = "MYSQL from VPC"
+    from_port        = 3306
+    to_port          = 3306 
+    protocol         = "tcp"
+    cidr_blocks      = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ecomm-db-firewall"
+  }
+}
