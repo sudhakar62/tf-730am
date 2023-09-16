5b1f04bca58475d915afb41e3d3c3bf320f76708
sudhakar62 <sudhakar6294@gmail.com>
2023-09-17 00:17:15 +0530

    create private route table

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -61,3 +61,13 @@ resource "aws_route_table_association" "ecomm-pub-association" {
   subnet_id      = aws_subnet.ecomm-pub-sn.id
   route_table_id = aws_route_table.ecomm-pub-rt.id
 }
+
+#Private Route Table
+resource "aws_route_table" "ecomm-pvt-rt" {
+  vpc_id = aws_vpc.ecomm.id
+
+  tags = {
+    Name = "ecomm-private-route-table"
+  }
+}
+
