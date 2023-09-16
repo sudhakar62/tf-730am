c16fa0e6a3875ebb7a7dafaab9a2d5e7802fdcfd
sudhakar62 <sudhakar6294@gmail.com>
2023-09-16 23:59:19 +0530

    Create public subnet

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -7,3 +7,16 @@ resource "aws_vpc" "ecomm" {
     Name = "ecomm-vpc"
   }
 }
+
+#Public Subnet
+resource "aws_subnet" "ecomm-pub-sn" {
+  vpc_id     = aws_vpc.ecomm.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-2b"
+  map_public_ip_on_launch = "true"
+
+  tags = {
+    Name = "ecomm-public-subnet"
+  }
+}
+
